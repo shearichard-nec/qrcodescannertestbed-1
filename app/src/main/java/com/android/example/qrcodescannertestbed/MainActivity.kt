ebded073335b8312d8f0462849e68a54ee676110
Richard Shea <richard.shea@nec.co.nz>
2024-08-14 16:25:26 +1200

    QR code now working and displaying QR code to a toast

--- app/src/main/java/com/android/example/qrcodescannertestbed/MainActivity.kt
+++ app/src/main/java/com/android/example/qrcodescannertestbed/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.android.example.qrcodescannertestbed
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Start QRCodeScannerActivity when the app starts
+        val intent = Intent(this, QRCodeScannerActivity::class.java)
+        startActivity(intent)
+
+        // Finish the MainActivity so it doesn't remain in the back stack
+        finish()
     }
 }
+
